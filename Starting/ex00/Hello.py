5613162c4b4226472533d9832db720ace78d14ba
Soufiane Dahani <dahanissoufiane@gmail.com>
2026-02-11 13:58:46 +0100

    ex00

---
+++ Starting/ex00/Hello.py
@@ -0,0 +1,32 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+
+ft_list[1] = "World!"
+
+ft_tuple = list(ft_tuple)
+ft_tuple[1] = "France!"
+
+ft_set = list(ft_set)
+ft_set[1] = "Paris!"
+ft_set[0] = "Hello" # problem
+
+ft_dict = list(ft_dict)
+ft_dict.append("42Paris!")
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
+
+
+
+# Expected output:
+# $>python Hello.py | cat -e
+# ['Hello', 'World!']$
+# ('Hello', 'France!')$
+# {'Hello', 'Paris!'}$
+# {'Hello': '42Paris!'}$
+# $>
