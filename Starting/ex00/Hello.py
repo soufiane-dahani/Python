6dd6196c99b250fdd9d39f277edad8601320fe8b
Soufiane Dahani <dahanissoufiane@gmail.com>
2026-02-12 14:43:08 +0100

    edit ex00 and complete the ex01

--- Starting/ex00/Hello.py
+++ Starting/ex00/Hello.py
@@ -10,11 +10,11 @@ ft_tuple = list(ft_tuple)
 ft_tuple[1] = "France!"
 
 ft_set = list(ft_set)
+ft_set[0] = "Hello" # fix problem (set have no order)
 ft_set[1] = "Paris!"
-ft_set[0] = "Hello" # problem
 
 ft_dict = list(ft_dict)
-ft_dict.append("42Paris!")
+ft_dict.append("42Paris!") # list does not take the value of key
 
 print(ft_list)
 print(ft_tuple)
