d9fb863cc6f5ec8a0f3495353fb1eb2f7eae3778
Soufiane Dahani <dahanissoufiane@gmail.com>
2026-02-12 22:26:33 +0100

    ex04

--- Starting/ex04/whatis.py
+++ Starting/ex04/whatis.py
@@ -1,17 +1,17 @@
-# $> python whatis.py 14
-# I'm Even.
-# $>
-# $> python whatis.py -5
-# I'm Odd.
-# $>
-# $> python whatis.py
-# $>
-# $> python whatis.py 0
-# I'm Even.
-# $>
-# $> python whatis.py Hi!
-# AssertionError: argument is not an integer
-# $>
-# $> python whatis.py 13 5
-# AssertionError: more than one argument is provided
-# $>
+import sys
+print("__name__ is:", __name__)
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        pass
+    elif len(sys.argv) == 2:
+        try:
+            number = int(sys.argv[1])
+            print("My number is:", number)
+            if number % 2 == 0:
+                print("I'm Even.")
+            else:
+                print("I'm Odd.")
+        except ValueError:
+            print("AssertionError: argument is not an integer")
+    else:
+        print("AssertionError: more than one argument is provided")
